b7aa39614f61f130733d08d794331ef917b28acc
amitrovshitz <amitrovshitz@amitrovshitz.myguest.virtualbox.org>
2023-04-09 10:31:32 +0300

    part b init

---
+++ sources/card.cpp
@@ -0,0 +1,11 @@
+
+#include "card.hpp"
+#include <string>
+using namespace ariel;
+
+
+Card::Card(int number, std::string shape)
+{
+    this->shape = shape;
+    this->value = number;
+}
---
+++ sources/game.cpp
@@ -0,0 +1,31 @@
+#include "game.hpp"
+using namespace ariel;
+
+Game::Game(Player& pp1, Player& pp2): player1(pp1), player2(pp2){}
+
+void Game::playAll(){
+
+}
+
+void Game::printWiner(){
+
+}
+
+void Game::printStats(){
+
+}
+
+void Game::printLog(){
+
+}
+
+void Game::printLastTurn(){
+
+}
+
+void Game::playTurn(){
+
+}
+
+
+
